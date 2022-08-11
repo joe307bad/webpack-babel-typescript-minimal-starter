56751e525bcfa29ce98a8dd4be8e3d9256b92012
Joe Badaczewski <joebad@amazon.com>
2022-08-10 23:26:47 -0400

    Upgrading dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -3,46 +3,47 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src'),
+    entry: path.resolve(__dirname, 'src'),
+    mode: "development",
 
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
 
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json'],
-  },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json'],
+    },
 
-  module: {
-    rules: [{
-      test: /\.(ts|js)x?$/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            '@babel/env',
-            '@babel/typescript',
-          ],
-          plugins: ['@babel/proposal-class-properties'],
-        },
-      },
-      exclude: /node_modules/,
-    }],
-  },
+    module: {
+        rules: [{
+            test: /\.(ts|js)x?$/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        '@babel/env',
+                        '@babel/typescript',
+                    ],
+                    plugins: ['@babel/proposal-class-properties'],
+                },
+            },
+            exclude: /node_modules/,
+        }],
+    },
 
-  plugins: [
-    // Re-generate index.html with injected script tag.
-    // The injected script tag contains a src value of the
-    // filename output defined above.
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: './public/index.html',
-    }),
-    new ForkTsCheckerWebpackPlugin()
-  ],
+    plugins: [
+        // Re-generate index.html with injected script tag.
+        // The injected script tag contains a src value of the
+        // filename output defined above.
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+        }),
+        new ForkTsCheckerWebpackPlugin()
+    ],
 
-  devServer: {
-    contentBase: './dist',
-  },
+    devServer: {
+        static: './dist',
+    },
 };
